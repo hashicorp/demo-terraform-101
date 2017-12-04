1e705ca95867a432bc6b5eb34a494d20b117451a
Geoffrey Grosenbach <boss@topfunky.com>
2017-12-04 15:36:06 -0800

    Refactor to a module

--- main.tf
+++ main.tf
@@ -30,52 +30,20 @@ provider "aws" {
   region     = "${var.aws_region}"
 }
 
-resource "aws_key_pair" "training" {
-  key_name   = "${var.identity}-key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
+module "webserver" {
+  source = "./webserver"
 
-resource "aws_instance" "web" {
-  ami           = "${var.ami_id}"
-  instance_type = "t2.nano"
-  count         = "${var.total_webs}"
-
-  subnet_id              = "${var.subnet_id}"
-  vpc_security_group_ids = ["${var.security_group_id}"]
-
-  key_name = "${aws_key_pair.training.id}"
-
-  tags {
-    "Name"     = "web ${count.index+1}/${var.total_webs}"
-    "Identity" = "${var.identity}"
-    "Foo"      = "bar"
-    "Zip"      = "zap"
-  }
-
-  connection {
-    user        = "ubuntu"
-    private_key = "${file("~/.ssh/id_rsa")}"
-  }
-
-  provisioner "file" {
-    source      = "assets"
-    destination = "/tmp/"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo cp /tmp/assets/webapp /usr/local/bin/",
-      "sudo chmod +x /usr/local/bin/*",
-      "sudo cp /tmp/assets/webapp.service /lib/systemd/system/webapp.service",
-      "sudo service webapp start",
-    ]
-  }
+  total_webs        = 1
+  ami_id            = "${var.ami_id}"
+  subnet_id         = "${var.subnet_id}"
+  security_group_id = "${var.security_group_id}"
+  identity          = "${var.identity}"
 }
 
 output "public_ip" {
-  value = ["${aws_instance.web.*.public_ip}"]
+  value = "${module.webserver.public_ip}"
 }
 
 output "public_dns" {
-  value = ["${aws_instance.web.*.public_dns}"]
+  value = "${module.webserver.public_dns}"
 }
---
+++ webserver/main.tf
@@ -0,0 +1,59 @@
+variable "ami_id" {}
+
+variable "total_webs" {
+  default = 1
+}
+
+variable "subnet_id" {}
+variable "security_group_id" {}
+variable "identity" {}
+
+resource "aws_key_pair" "training" {
+  key_name   = "${var.identity}-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "${var.ami_id}"
+  instance_type = "t2.nano"
+  count         = "${var.total_webs}"
+
+  subnet_id              = "${var.subnet_id}"
+  vpc_security_group_ids = ["${var.security_group_id}"]
+
+  key_name = "${aws_key_pair.training.id}"
+
+  tags {
+    "Name"     = "web ${count.index+1}/${var.total_webs}"
+    "Identity" = "${var.identity}"
+    "Foo"      = "bar"
+    "Zip"      = "zap"
+  }
+
+  connection {
+    user        = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  provisioner "file" {
+    source      = "assets"
+    destination = "/tmp/"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo cp /tmp/assets/webapp /usr/local/bin/",
+      "sudo chmod +x /usr/local/bin/*",
+      "sudo cp /tmp/assets/webapp.service /lib/systemd/system/webapp.service",
+      "sudo service webapp start",
+    ]
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
