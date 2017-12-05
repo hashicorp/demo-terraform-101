3ef4e52775146b45618babfaf47d440f2a40973b
Geoffrey Grosenbach <boss@topfunky.com>
2017-12-04 16:24:41 -0800

    Set version for AWS provider

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ variable "total_webs" {
 # }
 
 provider "aws" {
+  version    = "~> 1.5"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
