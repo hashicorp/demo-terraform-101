0ca74da3386ef09c62c121a4d48081548efb094d
Geoffrey Grosenbach <boss@topfunky.com>
2018-01-25 15:45:54 -0800

    Tweak for use without SSH

--- main.tf
+++ main.tf
@@ -31,27 +31,10 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 module "server" {
   source = "./server"
 
   num_webs = "${var.num_webs}"
-  ami      = "${data.aws_ami.ubuntu.id}"
   identity = "${var.identity}"
 }
 
