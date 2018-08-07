df552eeaea674347e157f2504b5b3bbd2e8cd7ca
usmc686 <42185416+usmc686@users.noreply.github.com>
2018-08-07 12:04:26 -0700

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -6,7 +6,7 @@ variable "subnet_id" {}
 variable "vpc_security_group_id" {}
 variable "identity" {}
 variable "public_key" {}
-variable "private key" {}
+variable "private_key" {}
 
 resource "aws_key_pair" "training" {
   key_name   = "${var.identity}-key"
