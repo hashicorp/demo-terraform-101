a101299a226faed4994ac5b50898a68b3607373e
Geoffrey Grosenbach <boss@topfunky.com>
2019-06-17 18:02:40 -0700

    Final GCP Getting Started configuration

---
+++ getting-started/gcp/main.tf
@@ -0,0 +1,55 @@
+provider "google" {
+  project = "my-google-cloud-project"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  labels = {
+    billing_department = "education"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+
+  metadata_startup_script = "echo 'Hello Terraform on GCP!' > index.html; python -m SimpleHTTPServer 9000 &"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "terraform-firewall"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9000"]
+  }
+}
+
+output "private_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].network_ip
+}
+
+output "public_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
+
+output "website_url" {
+  value = "http://${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip}:9000"
+}
+
