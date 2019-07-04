c632e9992b95000d413775ca4c4afe3d4a03a169
Radek Simko <radek.simko@gmail.com>
2019-07-04 16:01:09 +0100

    Add definition of variables

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "identity" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_security_group_id" {
+  type = string
+}
