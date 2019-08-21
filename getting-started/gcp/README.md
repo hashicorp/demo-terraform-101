6c8ddacd1402ddbbef572b0df12191a46e11afdf
Robin Norwood <rnorwood@hashicorp.com>
2019-08-21 12:21:29 -0500

    Update initial GCP example code

---
+++ getting-started/gcp/README.md
@@ -0,0 +1,9 @@
+# Getting Started with Terraform on Google Cloud Platform
+
+This code accompanies the [Getting Started](https://learn.hashicorp.com/terraform/gcp/intro) guide on [learn.hashicorp.com](https://learn.hashicorp.com/).
+
+Check out the `after` branch for the final code:
+
+```shell
+git checkout after
+```
--- getting-started/gcp/main.tf
+++ getting-started/gcp/main.tf
@@ -1,6 +1,30 @@
 provider "google" {
-  project = "terraform-project"
+  credentials = file("GCP Review-bad9ce4e27a7.json")
+
+  project = "gcp-review-250516"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
