b848753b361387eec00ad196786518063710b0c3
Robin Norwood <rnorwood@hashicorp.com>
2019-08-21 12:24:37 -0500

    Correct version of main.tf

--- getting-started/gcp/main.tf
+++ getting-started/gcp/main.tf
@@ -1,30 +1,11 @@
 provider "google" {
-  credentials = file("GCP Review-bad9ce4e27a7.json")
+  credentials = file("<NAME>.json")
 
-  project = "gcp-review-250516"
+  project = "<PROJECT_ID>"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-
-  tags         = ["web", "dev"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
 }
