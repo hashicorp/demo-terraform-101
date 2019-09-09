7e2ab2bf9b56590844a36b6c12700f22d5c84ea0
name-is-john <55065904+name-is-john@users.noreply.github.com>
2019-09-09 12:19:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 0.12.0"
 }
-
 provider "aws" {
-  >= 2.27.0
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
 }
 
 module "server" {
