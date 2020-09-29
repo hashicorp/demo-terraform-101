1162778339ddb0082c2837778f84f57b4e8bb7ef
HashiCorp Training <training@hashicorp.com>
2020-09-29 14:05:56 +0000

    testing push with provider update"

--- server/main.tf
+++ server/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
+}
+
 resource "aws_key_pair" "default" {
   key_name   = "${var.identity}-key"
   public_key = var.public_key
