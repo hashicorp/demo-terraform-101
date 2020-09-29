42e23eb3aec1f62f1e57bca01d64ab8ece45b60c
Scott Thornton <38704968+scthornton@users.noreply.github.com>
2020-09-29 06:25:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
-}
-
 resource "aws_instance" "web" {
   # ...
+}
+
+provider "aws" {
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
 }
