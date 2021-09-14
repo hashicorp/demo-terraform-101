0463d3e741d38ae5101d3a5eb8452a1349340435
ecsd-matthew-song <matthew.song@ecs-digital.co.uk>
2021-09-14 16:37:15 +0800

    Fixed ami issue

--- 05_multi-provider/main.tf
+++ 05_multi-provider/main.tf
@@ -24,6 +24,17 @@ provider "aws" {
 data "github_ip_ranges" "test" {
 }
 
+data "aws_ami" "ubuntu_16_04" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
 resource "aws_security_group" "training" {
   name_prefix = var.namespace
 
@@ -50,7 +61,7 @@ resource "aws_key_pair" "training" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = var.ami
+  ami                    = data.aws_ami.ubuntu_16_04.image_id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.training.id]
 
