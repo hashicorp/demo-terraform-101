717d5b5f569b41b53f44c8eb124374dfa00f0728
kktf <107646905+kktf@users.noreply.github.com>
2022-06-16 16:22:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,8 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
 }
-
-provider "aws" {
-}
-
-module "server" {
-  source = "./server"
-
-  num_webs     = var.num_webs
-  identity     = var.identity
-  ami          = var.ami
-  ingress_cidr = var.ingress_cidr
-  public_key   = var.public_key
-  private_key  = var.private_key
-}
-
